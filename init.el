ebe94b9f6620a20c3b80002a12195dd60e8d67a5
KemalOeztuerk <ozturk.kemaleren@gmail.com>
2024-03-10 01:14:05 +0300

    C/C++ autocomplete, snippet added

--- init.el
+++ init.el
@@ -1,10 +1,5 @@
-(setq inhibit-splash-screen t)
 
-(set-face-attribute 'default nil :font "DejaVu Sans Mono")
 
-(add-hook 'window-setup-hook 'toggle-frame--maximized)
-(scroll-bar-mode -1)
-(electric-pair-mode 1)
 
 (setq package-install-upgrade-built-in t)
 ;; Initialize Melpa
@@ -15,9 +10,19 @@
       "http://melpa.org/packages/"))
 (package-initialize)
 
+
 ;;Dark Theme
 ;;(package-install 'ample-theme)
 (load-theme 'ample t)
+
+;;Basic adjustment
+(setq inhibit-splash-screen t)
+(add-hook 'window-setup-hook 'toggle-frame--maximized)
+(scroll-bar-mode -1)
+(electric-pair-mode 1)
+(global-display-line-numbers-mode t)
+(set-face-attribute 'default nil :font "DejaVu Sans Mono")
+
 
 (use-package helm
   :ensure t
@@ -40,6 +45,44 @@
  :config
  (dashboard-setup-startup-hook))
 
+;; C/C++
+
+(use-package irony
+  :ensure t
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options))
+
+(use-package irony
+  :ensure t
+  :config
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+
+  ;; Install the Irony server if not already installed
+  (defun my-irony-setup ()
+    (unless (irony--server-binary-valid-p)
+      (call-interactively #'irony-install-server)))
+
+  (add-hook 'irony-mode-hook 'my-irony-setup))
+
+(use-package company-irony
+  :ensure t
+  :config
+  (eval-after-load 'company
+    '(add-to-list 'company-backends 'company-irony)))
+
+
+(use-package irony-eldoc
+  :ensure t
+  :config
+  (add-hook 'irony-mode-hook 'irony-eldoc))
+
+
+
+
 
 
 
@@ -49,7 +92,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dashboard helm-projectile helm use-package projectile magit lsp-mode irony helm-dash flycheck company ample-theme)))
+   '(irony-eldoc projectile use-package magit lsp-mode helm-projectile helm-dash flycheck dashboard company-irony auto-complete ample-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
